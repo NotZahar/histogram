081ef15dde801c44e1dd2653bb9553e7e274026b
NotZahar <62999416+NotZahar@users.noreply.github.com>
2024-04-23 19:36:00 +0000

    Deploying to gh-pages from @ NotZahar/histogram@d0dda11ddeeaab8835a581319944d8fdeb47324e 🚀

---
+++ search/searchdata.js
@@ -0,0 +1,24 @@
+var indexSectionsWithContent =
+{
+  0: "mrП",
+  1: "mr",
+  2: "m",
+  3: "П"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "functions",
+  3: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "Указатель",
+  1: "Файлы",
+  2: "Функции",
+  3: "Страницы"
+};
+
